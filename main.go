80262962ad655695d248f541f4ea22c5e7025f7d
hogepiyo <s16054@tokyo.kosen-ac.jp>
2020-02-08 22:06:26 +0900

    index完成

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("pages/*.html")
+	router.StaticFile("/GitHub.png", "./img/GitHub_Logo_White.png")
+	router.StaticFile("/Qiita.png", "./img/logo-effect.png")
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{})
@@ -20,5 +22,5 @@ func main() {
 		ctx.HTML(200, "products.html", gin.H{})
 	})
 
-	router.Run(":443")
+	router.Run()
 }
